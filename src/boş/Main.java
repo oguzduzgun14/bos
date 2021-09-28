c620f4f31347940cad0ab59e0ecb9a8c73b8b5b0
oguzduzgun14 <oguzduzgun1414@gmail.com>
2021-09-28 14:34:05 +0300

    Initial commit

---
+++ src/boş/Main.java
@@ -0,0 +1,12 @@
+package boþ;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("boþ");
+		System.out.println("ful boþ");
+
+	}
+
+}
